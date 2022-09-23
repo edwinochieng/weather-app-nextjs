ce12442d998f9401b2bccaa814211bf47a744b52
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-09-23 22:54:13 +0300

    react-query provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import React from "react";
+import { QueryClient,Hydrate,QueryClientProvider } from "@tanstack/react-query";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [queryClient] = React.useState(() => new QueryClient())
+  return (
+    
+    <QueryClientProvider client={queryClient}>
+    <Hydrate state={pageProps.dehydratedState}>
+      <Component {...pageProps} />
+    </Hydrate>
+  </QueryClientProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
