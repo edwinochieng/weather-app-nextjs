049a235699bba47ac015ada43bc7dfa7f52e590e
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-09-23 23:40:57 +0300

    remove queryprovider

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,14 @@
 import "../styles/globals.css";
 import React from "react";
-import { QueryClient,Hydrate,QueryClientProvider } from "@tanstack/react-query";
+
 
 function MyApp({ Component, pageProps }) {
-  const [queryClient] = React.useState(() => new QueryClient())
+  
   return (
     
-    <QueryClientProvider client={queryClient}>
-    <Hydrate state={pageProps.dehydratedState}>
+    
       <Component {...pageProps} />
-    </Hydrate>
-  </QueryClientProvider>
+    
   );
 }
 
